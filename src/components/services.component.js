69a0da4de8993151a28b0304b495a5955deb5ff5
Jacob Tingler <jetingle@uno.edu>
2020-05-15 10:43:34 -0500

    services page

--- src/components/services.component.js
+++ src/components/services.component.js
@@ -2,6 +2,41 @@ import React, { Component } from "react";
 
 export default class Services extends Component {
   render() {
-    return <div></div>;
+    return (
+      <div>
+        <div className="page-section">
+          <table className="table table-dark">
+            <tr>
+              <th>Service</th>
+              <th>Price</th>
+            </tr>
+            <tr>
+              <td>Haircut</td>
+              <td>$25.00</td>
+            </tr>
+            <tr>
+              <td>Haircut and Beard</td>
+              <td>$30.00</td>
+            </tr>
+            <tr>
+              <td>Children's Haircut</td>
+              <td>$20.00</td>
+            </tr>
+            <tr>
+              <td>Lineup</td>
+              <td>$10.00</td>
+            </tr>
+            <tr>
+              <td>Lineup with Beard</td>
+              <td>$15.00</td>
+            </tr>
+            <tr>
+              <td>Facial</td>
+              <td>$15.00</td>
+            </tr>
+          </table>
+        </div>
+      </div>
+    );
   }
 }

afb7d5e177e4a42a1432f232f9f6ffd172e66627
Jacob Tingler <jetingle@uno.edu>
2020-05-15 13:49:44 -0500

    build

---
+++ build/precache-manifest.ee2daf45179a43ffc5490ca9ef84c376.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f97540f3338a25806917844684fd8369",
+    "url": "/index.html"
+  },
+  {
+    "revision": "29ab0fba1552de9f31bb",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "b6d4b8fb4324f9df55d4",
+    "url": "/static/css/main.c1ccd6b5.chunk.css"
+  },
+  {
+    "revision": "29ab0fba1552de9f31bb",
+    "url": "/static/js/2.9e87b590.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.9e87b590.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b6d4b8fb4324f9df55d4",
+    "url": "/static/js/main.1282c7f4.chunk.js"
+  },
+  {
+    "revision": "14c9fe4f348d9933b744",
+    "url": "/static/js/runtime-main.afddb9f9.js"
+  },
+  {
+    "revision": "bf4fe324b970adc5e747838309fb38f3",
+    "url": "/static/media/1.bf4fe324.jpg"
+  },
+  {
+    "revision": "ef064cfc9f65001eabb6a23d056ab770",
+    "url": "/static/media/header-bg.ef064cfc.jpg"
+  },
+  {
+    "revision": "f40ed5b1e791e9d2479ba8b5e67f2df8",
+    "url": "/static/media/map-image.f40ed5b1.png"
+  }
+]);

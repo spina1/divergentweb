4171986f29701dd2ac6233babfc4ce7240193a47
spina1 <spina1@uno.edu>
2020-05-15 09:13:55 -0500

    backend complete

--- src/components/services.component.js
+++ src/components/services.component.js
@@ -2,10 +2,6 @@ import React, { Component } from "react";
 
 export default class Services extends Component {
   render() {
-    return (
-      <div>
-        <p>You are on the Services component!</p>
-      </div>
-    );
+    return <div></div>;
   }
 }
--- src/components/signup.component.js
+++ src/components/signup.component.js
@@ -1,7 +1,85 @@
 import React, { Component } from "react";
+import axios from "axios";
 
 export default class Book extends Component {
+  constructor(props) {
+    super(props);
+
+    this.onChangeClientName = this.onChangeClientName.bind(this);
+    this.onChangeClientPhone = this.onChangeClientPhone.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+
+    this.state = {
+      clientname: "",
+      clientphone: "",
+    };
+  }
+
+  onChangeClientName(e) {
+    this.setState({
+      clientname: e.target.value,
+    });
+  }
+  onChangeClientPhone(e) {
+    this.setState({
+      clientphone: e.target.value,
+    });
+  }
+
+  onSubmit(e) {
+    e.preventDefault();
+    const newClient = {
+      clientname: this.state.clientname,
+      clientphone: this.state.clientphone,
+    };
+    console.log(newClient);
+    axios
+      .post("http://localhost:4000/clients/add", newClient)
+      .then((res) => console.log(res.data));
+
+    this.setState({
+      clientname: "",
+      clientphone: "",
+    });
+  }
+
   render() {
-    return <div>book</div>;
+    return (
+      <div>
+        <div className="page-section">
+          <h3>Sign Up For Notifications</h3>
+          <form onSubmit={this.onSubmit}>
+            <div className="form-group">
+              <label>Name: </label>
+              <input
+                type="text"
+                required
+                className="form-control"
+                value={this.state.clientname}
+                onChange={this.onChangeClientName}
+              />
+            </div>
+            <div className="form-group">
+              <label>Phone: </label>
+              <input
+                type="text"
+                required
+                className="form-control"
+                value={this.state.clientphone}
+                onChange={this.onChangeClientPhone}
+              />
+            </div>
+
+            <div className="form-group">
+              <input
+                type="submit"
+                value="Sign Up"
+                className="btn btn-primary"
+              />
+            </div>
+          </form>
+        </div>
+      </div>
+    );
   }
 }
